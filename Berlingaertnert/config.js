248f7a32fb0926bcf15de242c4a9ff2054ff76eb
Lisa-Stubert <lisastubert@web.de>
2025-03-01 18:52:31 +0100

    feat: add VectorTile Basemap

--- Berlingaertnert/config.js
+++ Berlingaertnert/config.js
@@ -1,29 +1,36 @@
 // eslint-disable-next-line no-unused-vars
 const Config = {
-    alerting: {
-        fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json"
-    },
-    namedProjections: [
-        ["EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+  alerting: {
+    fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json",
+  },
+  namedProjections: [
+    [
+      "EPSG:25833",
+      "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
     ],
-    layerConf: "./resources/services.json",
-    restConf: "./resources/rest-services.json",
-    styleConf: "./resources/style_v3.json",
-    wfsImgPath: "./resources/img/",
-    portalLanguage: {
-        enabled: true,
-        debug: false,
-        languages: {
-            de: "Deutsch",
-            en: "English",
-            es: "Español",
-            it: "Italiano",
-            pt: "Português",
-            ru: "Русский",
-            tr: "Türkçe",
-            ua: "Українська"
-        },
-        fallbackLanguage: "de",
-        changeLanguageOnStartWhen: ["querystring", "localStorage", "htmlTag"]
-    }
+    [
+      "EPSG:3857",
+      "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs",
+    ],
+  ],
+  layerConf: "./resources/services.json",
+  restConf: "./resources/rest-services.json",
+  styleConf: "./resources/style_v3.json",
+  wfsImgPath: "./resources/img/",
+  portalLanguage: {
+    enabled: true,
+    debug: false,
+    languages: {
+      de: "Deutsch",
+      en: "English",
+      es: "Español",
+      it: "Italiano",
+      pt: "Português",
+      ru: "Русский",
+      tr: "Türkçe",
+      ua: "Українська",
+    },
+    fallbackLanguage: "de",
+    changeLanguageOnStartWhen: ["querystring", "localStorage", "htmlTag"],
+  },
 };
