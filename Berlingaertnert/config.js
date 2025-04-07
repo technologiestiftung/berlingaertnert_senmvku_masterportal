17019a33a45947c876c796545f8da30d41d05a56
basicyachtclub <79273303+basicyachtclub@users.noreply.github.com>
2025-04-07 20:23:41 +0200

    add real data. hide geometry in menu. adjust icon size. add new logo. add default point style.

--- Berlingaertnert/config.js
+++ Berlingaertnert/config.js
@@ -17,6 +17,7 @@ const Config = {
   restConf: "./resources/rest-services.json",
   styleConf: "./resources/style_v3.json",
   wfsImgPath: "./resources/img/",
+  ignoredKeys: ["BOUNDEDBY", "SHAPE", "SHAPE_LENGTH", "SHAPE_AREA", "OBJECTID", "GLOBALID", "GEOMETRY", "SHP", "SHP_AREA", "SHP_LENGTH","GEOM"],
   portalLanguage: {
     enabled: true,
     debug: false,
