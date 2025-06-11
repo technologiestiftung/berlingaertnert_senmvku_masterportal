ea6cd58741e5bb7c8c2d5e6797d0174e81d22974
basicyachtclub <79273303+basicyachtclub@users.noreply.github.com>
2025-06-11 18:48:15 +0200

    fix - workaround for legend item not showing if only single entry is rendered on map initially.

--- berlingaertnert/config.js
+++ berlingaertnert/config.js
@@ -10,7 +10,11 @@ const Config = {
     ],
     [
       "EPSG:3857",
-      "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs",
+      "+title=WGS 84 (meter) +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs",
+    ],
+    [
+      "EPSG:4326",
+      "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees +no_defs +type=crs",
     ],
   ],
   layerConf: "./resources/services.json",
